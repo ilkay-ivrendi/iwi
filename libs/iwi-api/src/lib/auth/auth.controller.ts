af22e613617f92532f0032cfd7edf4b906477a6c
ilkay-ivrendi <ilkay.ivrendi@gmail.com>
2020-02-01 03:02:53 +0100

    jwt strategy mock

--- libs/iwi-api/src/lib/auth/auth.controller.ts
+++ libs/iwi-api/src/lib/auth/auth.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, UseGuards, Post, Request } from '@nestjs/common';
+import { Controller, UseGuards, Post, Request, Get } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @UseGuards(AuthGuard('local'))
-  @Post('auth/login')
+  @Post('login')
   async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
--- libs/iwi-api/src/lib/auth/auth.module.ts
+++ libs/iwi-api/src/lib/auth/auth.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { AuthService, jwtConstants } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { LocalStrategy } from './local.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
+  imports: [
+    UsersModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1060s' }
+    })
+  ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
-  imports: [UsersModule, PassportModule]
+  providers: [AuthService, LocalStrategy, JwtStrategy]
 })
 export class AuthModule {}
