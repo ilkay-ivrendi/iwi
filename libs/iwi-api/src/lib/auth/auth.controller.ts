f62e5626eb06219f5943758a7f56afe79bae8c0f
ilkay-ivrendi <ilkay.ivrendi@gmail.com>
2020-02-01 02:45:54 +0100

    passport strategy implemented

--- libs/iwi-api/src/lib/auth/auth.controller.ts
+++ libs/iwi-api/src/lib/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, Post, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
+}
--- libs/iwi-api/src/lib/auth/auth.module.ts
+++ libs/iwi-api/src/lib/auth/auth.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  controllers: [AuthController]
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy],
+  imports: [UsersModule, PassportModule]
 })
 export class AuthModule {}
